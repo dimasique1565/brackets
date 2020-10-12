90b22f8c6957183ab37e90797e676970057653f4
dimasique1565 <velkov2000@list.ru>
2020-10-12 05:08:33 +0300

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    if(typeof bracketsConfig[i] != "string")bracketsConfig[i] = bracketsConfig[i].join('');
+  }
+  for(let i = 0; i < 100; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      str = str.replace(bracketsConfig[j], '');
+    }
+  }
+  if(str == '') return true;
+  else return false;
 }
